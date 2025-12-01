1ffa07d5432ef9a611684bb01dd654078a3d32a9
digarciahuerta <israeldavid998@gmail.com>
2025-12-01 17:39:58 -0600

    Finds largest and smallest in array

---
+++ question7.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() 
+{
+    int arr[10];
+    printf("Enter 10 integers:\n");
+    for (int i = 0; i < 10; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+    int max = arr[0], min = arr[0];
+    for (int i = 1; i < 10; i++) 
+    {
+        if (arr[i] > max) max = arr[i];
+        if (arr[i] < min) min = arr[i];
+    }
+    printf("Largest = %d\n", max);
+    printf("Smallest = %d\n", min);
+    return 0;
+}
