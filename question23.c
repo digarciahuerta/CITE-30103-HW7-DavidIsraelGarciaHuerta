ff6c865f20287b10b969b1dc02a4fdb06a8dea66
digarciahuerta <israeldavid998@gmail.com>
2025-12-02 17:39:32 -0600

    gets integers and takes average

---
+++ question23.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+    int n, *arr;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    arr = (int*)malloc(n * sizeof(int));
+    if (arr == NULL) 
+    {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+    printf("Enter %d integers:\n", n);
+    float sum = 0;
+    for (int i = 0; i < n; i++) 
+    {
+        scanf("%d", arr + i);
+        sum += arr[i];
+    }
+    printf("Average = %.2f\n", sum / n);
+    free(arr);
+    return 0;
+}
+
