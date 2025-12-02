c3dc91c38d056a6d1101da3ff01a86ef6d65e58a
digarciahuerta <israeldavid998@gmail.com>
2025-12-01 21:12:49 -0600

    reverses an array and displays it

---
+++ question9.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() 
+{
+    int arr[5];
+    printf("Enter 5 integers:\n");
+    for (int i = 0; i < 5; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < 2; i++) 
+    {
+        int temp = arr[i];
+        arr[i] = arr[4 - i];
+        arr[4 - i] = temp;
+    }
+    printf("Reversed array:\n");
+    for (int i = 0; i < 5; i++) 
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
